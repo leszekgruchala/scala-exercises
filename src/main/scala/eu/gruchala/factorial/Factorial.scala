9525e4f2d45fa5ff54907b7f7d93473bbff602c4
Leszek Gruchala <leszek@gruchala.eu>
2018-04-30 18:58:26 +0200

    Factorials

--- src/main/scala/eu/gruchala/factorial/Factorial.scala
+++ src/main/scala/eu/gruchala/factorial/Factorial.scala
@@ -1,10 +1,21 @@
 package eu.gruchala.factorial
 
 object Factorial {
+
   def factorial(n: Int): Long = {
     def loop(rest: Int, acc: Long): Long = {
       if (rest == 0) acc
       else loop(rest - 1, acc * rest)
+    }
+
+    loop(n, 1)
+  }
+
+  def factorial2(n: Int): Long = {
+
+    def loop(rest: Int, acc: Long): Long = rest match {
+      case 0 => acc
+      case r => loop(r - 1, r * acc)
     }
 
     loop(n, 1)
--- src/test/scala/eu/gruchala/factorial/FactorialSpec.scala
+++ src/test/scala/eu/gruchala/factorial/FactorialSpec.scala
@@ -6,10 +6,16 @@ import eu.gruchala.factorial.Factorial._
 class FactorialSpec extends BaseSpec {
 
   "Factorial" should {
-    "do the job" in {
-      factorial(2) shouldEqual 4
-      factorial(4) shouldEqual 16
+    "factorial" in {
+      factorial(2) shouldEqual 2
+      factorial(4) shouldEqual 24
       factorial(20) shouldEqual 2432902008176640000L
+    }
+
+    "factorial2" in {
+      factorial2(2) shouldEqual 2
+      factorial2(4) shouldEqual 24
+      factorial2(20) shouldEqual 2432902008176640000L
     }
   }
 }
