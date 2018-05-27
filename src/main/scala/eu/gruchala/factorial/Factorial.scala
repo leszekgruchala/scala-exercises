ed952b2c3d981379d974fc11d5aca9d9dd035e38
Leszek Gruchala <leszek@gruchala.eu>
2018-05-27 18:15:30 +0200

    Add tailrec annotation to Factorial

--- src/main/scala/eu/gruchala/factorial/Factorial.scala
+++ src/main/scala/eu/gruchala/factorial/Factorial.scala
@@ -1,8 +1,11 @@
 package eu.gruchala.factorial
 
+import scala.annotation.tailrec
+
 object Factorial {
 
   def factorial(n: Int): Long = {
+    @tailrec
     def loop(rest: Int, acc: Long): Long = {
       if (rest == 0) acc
       else loop(rest - 1, acc * rest)
@@ -12,7 +15,7 @@ object Factorial {
   }
 
   def factorial2(n: Int): Long = {
-
+    @tailrec
     def loop(rest: Int, acc: Long): Long = rest match {
       case 0 => acc
       case r => loop(r - 1, r * acc)
